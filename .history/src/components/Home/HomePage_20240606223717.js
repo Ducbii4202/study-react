ef3dc22df0f384c0e9d42be60b1ba4ca9af0e71a
Ducbii4202 <144377938+Ducbii4202@users.noreply.github.com>
2024-06-06 23:00:58 +0700

    Design Homepage

--- .history/src/components/Home/HomePage_20240606223717.js
+++ .history/src/components/Home/HomePage_20240606223717.js
@@ -4,7 +4,7 @@ import videoHomepage from "../../assets/video-homepage.mp4";
 const HomePage = (props) => {
   return (
     <div className="homepage-container">
-      <video autoPlay loop muted>
+      <video>
         <source src={videoHomepage} type="video/mp4" />
       </video>
     </div>
--- .history/src/components/Home/HomePage_20240606223815.js
+++ .history/src/components/Home/HomePage_20240606223815.js
@@ -4,7 +4,7 @@ import videoHomepage from "../../assets/video-homepage.mp4";
 const HomePage = (props) => {
   return (
     <div className="homepage-container">
-      <video autoPlay loop muted>
+      <video width="750" height="500" controls>
         <source src={videoHomepage} type="video/mp4" />
       </video>
     </div>
