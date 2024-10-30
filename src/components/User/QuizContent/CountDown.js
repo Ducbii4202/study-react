3724bf7b51d76b1cc17c7ccec3107dc02762999d
Ducbii4202 <144377938+Ducbii4202@users.noreply.github.com>
2024-10-30 13:46:09 +0700

    CountDown timer with React

---
+++ src/components/User/QuizContent/CountDown.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+const CountDown = (props) => {
+  const [count, setCount] = useState(300);
+  const { onTimeUp } = props;
+  useEffect(() => {
+    if (count === 0) {
+      onTimeUp();
+      return;
+    }
+    const timer = setInterval(() => {
+      setCount(count - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [count]);
+
+  const toHHMMSS = (secs) => {
+    const sec_num = parseInt(secs, 10);
+    const hours = Math.floor(sec_num / 3600);
+    const minites = Math.floor(sec_num / 60) % 60;
+    const seconds = sec_num % 60;
+
+    return [hours, minites, seconds]
+      .map((v) => (v < 10 ? "0" + v : v))
+      .filter((v, i) => v !== "00" || i > 0)
+      .join(":");
+  };
+  return <div className="count-down container">{toHHMMSS(count)}</div>;
+};
+
+export default CountDown;
